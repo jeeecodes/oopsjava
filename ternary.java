f58e62dce0ea01935591e09231c3168298f57b42
jeeecodes <jeegeo123@gmail.com>
2024-08-31 07:28:13 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,11 @@
+public class ternary {
+    public static void main(String[] args) {
+        int x,y;
+        x=20;
+        y=(x==1)?60:90;
+        System.out.println("the value of y is "+y);
+        y=(x==20)?60:90;
+        System.out.println("the valuebof y is"+y);
+    }
+    
+}
